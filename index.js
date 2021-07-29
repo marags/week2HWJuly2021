ffe377ba788b74e4e223312006c6656cb6234f39
Michael Ragone <mikeragone82@gmail.com>
2021-07-29 14:14:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ function picture_move (next_or_prev) {
     console.log("Button Clicked: currentPositon = " + current_position)
     let update = document.getElementById("js_image")
     
-    //NextOrPrev is true, Next button was clicked, move to right (- pictureWidth) if not at the last picture
+    //next_or_prev is true (next button was selected) update current position + 1 if less than picture count (total pictures in carousel)
     if (next_or_prev && current_position < picture_count) {
         current_position += 1
         console.log("NEXT: moving to next picture " + picture_path + current_position + picture_end)
         update.setAttribute("src", picture_path + current_position + picture_end)
     }
-    // Else, Prev button was clicked, move to the left (+ pictureWidth) if not at the first picture
+    //next_or_prev is false (prev button was selected) update current position - 1 if more than picture count (1)
     if (!next_or_prev && current_position > 1) {
         current_position -= 1
         console.log("PREV: moving to prev picture " + picture_path + current_position + picture_end)
